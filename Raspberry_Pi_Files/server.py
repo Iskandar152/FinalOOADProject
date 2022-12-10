5c5b4248826fe9d1b3bad057e90af00eec830cff
Iskandar Shoyusupov <58271600+Iskandar152@users.noreply.github.com>
2022-12-09 23:39:55 -0700

    Update server.py

--- Raspberry_Pi_Files/server.py
+++ Raspberry_Pi_Files/server.py
@@ -3,8 +3,15 @@ import sys
 from toneplayer import Tone_Player
 from noteCreator import noteCreator
 from Note import Note
+'''
+This server class handles the creation of a TCP socket 
+that awaits connections from a client, and receives data.
+In this case the received data will be about the specific notes that 
+were pressed on the ios side 
+'''
 class Server:
     def __init__(self,host,port):
+        #Start the socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address = (host, port)
         self.connection = ""
@@ -13,21 +20,26 @@ class Server:
 
     def start_server(self):
         print("Starting Up Server")
+        #Bind the socket to the server address which is based on the host and port 
         self.sock.bind(self.server_address)
         self.sock.listen(1)
         return True
     def get_client(self):
+        #Wait for a client to connect (program blocks while it waits, there is no timeout)
         print("Waiting for Client...")
         self.connection, self.client = self.sock.accept()
         print("Received Connection From: " + str(self.client))
         return True
     def receive_tones(self):
+        #If we do receive a connection we start taking in the data 
         print("Receiving Tones")
         while True:
             data = self.connection.recv(16)
             if(data):
+                #Data is in binary format and needs to be converted using utf-8
                 note = data.decode("utf-8") 
                 print("Received: " + str(data))
+                #Note creator deciphers the data into notes and then we play the note 
                 note_maker = noteCreator(data)
                 note = note_maker.create_Note()
                 note.play()
