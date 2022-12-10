28e10bf0a45c4cc80da7484ab3ab8ff20544d336
Iskandar Shoyusupov <58271600+Iskandar152@users.noreply.github.com>
2022-12-09 23:42:53 -0700

    Update noteCreator.py

--- Raspberry_Pi_Files/noteCreator.py
+++ Raspberry_Pi_Files/noteCreator.py
@@ -10,11 +10,16 @@ class noteCreator:
         self.columnList = []
         self.note_factory = noteFactory()
     def split(self):
+        #The note data comes in as a positionX,screenWidth data
+        #We split this data into positionX and screenWidth seperately and save it
+        #A colon (:) is used as a delimiter 
         split_string = self.data.split(b':')
         self.tone_x = int(split_string[0])
         self.screen_width = m.floor(float(split_string[1]))
 
     def note_divider(self):
+        #Based on the positionx of the note pressed and the screen width 
+        #We can determine the list of potential note positions 
         tileLength = (self.screen_width - 20)/12
         self.key_Width = tileLength
         currX = (tileLength/2) + 10
@@ -23,6 +28,9 @@ class noteCreator:
             self.columnList.append(m.floor(currX))
             currX += tileLength
     def create_Note(self):
+        #Note creator, we utilize a note_factory to figure out which 
+        #note we are going to get basde on the position the note was in 
+        #on the x axis 
         self.split()
         self.note_divider()
         note = Note()
