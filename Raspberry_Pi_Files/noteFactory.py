4eb7333c7a34f562b966dcb20bf1f801d37dfcae
Iskandar Shoyusupov <58271600+Iskandar152@users.noreply.github.com>
2022-12-09 23:41:18 -0700

    Update noteFactory.py

--- Raspberry_Pi_Files/noteFactory.py
+++ Raspberry_Pi_Files/noteFactory.py
@@ -1,10 +1,14 @@
 from Note import Note
 class noteFactory:
     def __init__(self):
+        #Notes that we can select from 
         self.notes = ['A', 'B', 'C', 'D', 'E', 'F', 'C#', 'D#', 'F#', 'G#', 'A#', 'B#']
     def create_note(self,note_pos, note_list):
         return_note = Note()
         i = 0
+        #Based on the note position, we select a note 
+        #from self.notes based on where the pressed note was within the 
+        #note_list, so it's index
         for note_range in note_list:
             if note_range == note_pos:
                 return_note.note_name = self.notes[i]
