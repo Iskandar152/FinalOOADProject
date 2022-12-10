50370f72affa47d44b0bf2766da53a55535ac624
Iskandar Shoyusupov <58271600+Iskandar152@users.noreply.github.com>
2022-12-09 23:37:47 -0700

    Update toneplayer.py

--- Raspberry_Pi_Files/toneplayer.py
+++ Raspberry_Pi_Files/toneplayer.py
@@ -1,10 +1,17 @@
 import musicalbeeps
+'''
+Musical beeps is a library that can musical tones, 
+tone player is a class that runs the musical beeps 
+commands to play certain notes
+'''
 class Tone_Player:
     def __init__(self):
         print("Starting Tone Player")
         self.player = musicalbeeps.Player(volume = 0.7, mute_output = False)
-        
+    
     def play_tone(self, note, octave, time):
+        #User can define the note, octave, and the time interval that notes will play at
+        #for now the time interval is going to be constant at 1 second on the server side 
         note_descr = ""
         note_descr += str(note)
         note_descr += str(octave)
