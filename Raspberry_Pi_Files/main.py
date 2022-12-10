dd316e817a02a9347db6f58f2a81ee37cbdbca4e
Iskandar Shoyusupov <58271600+Iskandar152@users.noreply.github.com>
2022-12-09 23:43:38 -0700

    Update main.py

--- Raspberry_Pi_Files/main.py
+++ Raspberry_Pi_Files/main.py
@@ -7,10 +7,13 @@ from toneplayer import Tone_Player
 def main():
     
     print("START")
+    #Start the server on the devices IP and some host 
     server = Server("10.202.147.104",9999)
     server.start_server()
     while True:
+        #Continously check for a client connection
         if(server.get_client()):
+            #If the client connected, start receiving tones 
             server.receive_tones()
     
 
